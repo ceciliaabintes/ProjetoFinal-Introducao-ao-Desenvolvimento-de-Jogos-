6a0a3ad0e17d3e4bf46cbd88f663127ecbea7328
ceciliaabintes_ <142744298+ceciliaabintes@users.noreply.github.com>
2026-06-30 22:31:53 -0300

    .

--- TrilhaPerdida/Assets/Scripts/MenuPrincipal.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-using UnityEngine.SceneManagement;
-
-public class MenuPrincipal : MonoBehaviour
-{
-    public void Jogar()    => SceneManager.LoadScene("Cutscene01");
-    public void Sair()     { Application.Quit(); Debug.Log("Saindo..."); }
-    public void Creditos() => SceneManager.LoadScene("Creditos");
-}
